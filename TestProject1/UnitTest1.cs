1727f3df84f4a43d252d1d128723ed6011ad36b8
hassan4702 <muhammadhassanshakil35066@gmail.com>
2023-06-20 20:18:39 +0500

    commit 3

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -28,22 +28,22 @@ namespace TestProject1
             Actions actions = new Actions(driver);
             driver.Url = "https://www.daraz.pk/";
             driver.FindElement(By.LinkText("LOGIN")).Click();
-            driver.FindElement(By.CssSelector("#container > div > div:nth-child(2) > form > div > div.mod-login-col1 > div.mod-input.mod-login-input-loginName.mod-input-loginName > input[type=text]")).SendKeys("muhammadhassanshakil35066" +
-                "@gmail.com");
-            driver.FindElement(By.XPath("//*[@id=\"container\"]/div/div[2]/form/div/div[1]/div[2]/input")).SendKeys("altergate123");
-            driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[2]/div[1]/button")).Click();
-            Thread.Sleep(10000);
-            driver.FindElement(By.XPath("/html/body/div[1]/div/div/div[1]/div/div/div[1]/div/div[6]/span")).Click();
-            Thread.Sleep(1000);
-            driver.FindElement(By.XPath("//*[@id=\"lzdMyAccountPop\"]/div/ul/li[6]/a")).Click();
-            driver.FindElement(By.LinkText("LOGIN")).Click();
-            driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[2]/div[2]/div/div[2]/button[2]")).Click();
-            driver.SwitchTo().Window(driver.WindowHandles[1]);
-            IWebElement google_email = driver.FindElement(By.Id("identifierId"));
-            google_email.SendKeys("muhammadhassanshakil35066@gmail.com");
-            google_email.SendKeys(Keys.Enter);
-            Thread.Sleep(3000);
-            driver.SwitchTo().Window(driver.WindowHandles[0]);
+            //driver.FindElement(By.CssSelector("#container > div > div:nth-child(2) > form > div > div.mod-login-col1 > div.mod-input.mod-login-input-loginName.mod-input-loginName > input[type=text]")).SendKeys("muhammadhassanshakil35066" +
+            //    "@gmail.com");
+            //driver.FindElement(By.XPath("//*[@id=\"container\"]/div/div[2]/form/div/div[1]/div[2]/input")).SendKeys("altergate123");
+            //driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[2]/div[1]/button")).Click();
+            //Thread.Sleep(10000);
+            //driver.FindElement(By.XPath("/html/body/div[1]/div/div/div[1]/div/div/div[1]/div/div[6]/span")).Click();
+            //Thread.Sleep(1000);
+            //driver.FindElement(By.XPath("//*[@id=\"lzdMyAccountPop\"]/div/ul/li[6]/a")).Click();
+            //driver.FindElement(By.LinkText("LOGIN")).Click();
+            //driver.FindElement(By.XPath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[2]/div[2]/div/div[2]/button[2]")).Click();
+            //driver.SwitchTo().Window(driver.WindowHandles[1]);
+            //IWebElement google_email = driver.FindElement(By.Id("identifierId"));
+            //google_email.SendKeys("muhammadhassanshakil35066@gmail.com");
+            //google_email.SendKeys(Keys.Enter);
+            //Thread.Sleep(3000);
+            //driver.SwitchTo().Window(driver.WindowHandles[0]);
             driver.FindElement(By.CssSelector("#container > div > div:nth-child(2) > form > div > div.mod-login-col1 > div.mod-input.mod-login-input-loginName.mod-input-loginName > input[type=text]")).SendKeys("muhammadhassanshakil35066" +
                 "@gmail.com");
             driver.FindElement(By.XPath("//*[@id=\"container\"]/div/div[2]/form/div/div[1]/div[2]/input")).SendKeys("altergate123");
@@ -88,8 +88,10 @@ namespace TestProject1
             actions.SendKeys(Keys.PageUp).Build().Perform();
             Thread.Sleep(1000);
             actions.SendKeys(Keys.PageUp).Build().Perform();
-            
 
+            driver.FindElement(By.XPath("/html/body/div[3]/div/div/div/button")).Click();
+            driver.FindElement(By.XPath("/html/body/div[3]/div/div/div/span/div[1]/div[2]/div/div/div[1]/div[1]")).Click();
+            driver.FindElement(By.XPath("/html/body/div[3]/div/div/div/span/div[2]/form/div/textarea")).SendKeys("Thank you");
 
 
         }
